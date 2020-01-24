2e2d5dfc1059360d0eccbfc70dfb83079c50be1b
ttbarnes <tony@tonybarnes.me>
2020-01-24 09:46:55 +0000

    basic graphql logging

--- config/graphql/graphql.js
+++ config/graphql/graphql.js
@@ -1,6 +1,22 @@
 const { ApolloServer } = require('apollo-server-express');
+const { print } = require('graphql');
 const typeDefs = require('./schema');
 const resolvers = require('./resolvers');
+
+/* eslint-disable class-methods-use-this */
+class BasicLogging {
+  requestDidStart({ queryString, parsedQuery, variables }) {
+    const query = queryString || print(parsedQuery);
+    console.log('GQL query', query);
+    console.log('GQL vars', JSON.stringify(variables));
+  }
+
+  willSendResponse({ graphqlResponse }) {
+    console.log('GQL res', JSON.stringify(graphqlResponse));
+  }
+}
+/* eslint-enable class-methods-use-this */
+
 
 const graphql = new ApolloServer({
   typeDefs,
@@ -8,7 +24,8 @@ const graphql = new ApolloServer({
   formatError: ({ message, path }) => ({
     message,
     path
-  })
+  }),
+  extensions: [() => new BasicLogging()]
 });
 
 module.exports = graphql;
