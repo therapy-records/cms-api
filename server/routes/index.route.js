2c9a78306b6750c630302f792c9028f90f761556
ttbarnes <tony@tonybarnes.me>
2020-03-11 17:11:38 +0000

    remove gigs REST endpoints

--- server/routes/index.route.js
+++ server/routes/index.route.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const authRoutes = require('./auth.route');
-const gigsRoutes = require('./gigs.route');
 const journalismRoutes = require('./journalism.route');
 const newsRoutes = require('./news.route');
 const pressRoutes = require('./press.route');
@@ -11,8 +10,6 @@ const config = require('../../config/env');
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.use('/auth', authRoutes);
-
-router.use('/gigs', gigsRoutes);
 
 router.use('/journalism', journalismRoutes);
 
